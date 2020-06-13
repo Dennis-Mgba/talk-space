11a57b45c68aec7f78b5255f47f5a0f506b2803e
Dennis Mgbah <dennismgbah@gmail.com>
2020-06-13 21:04:26 +0100

    Displayed individual discussion with it's replies on a page

--- resources/views/discussions/show.blade.php
+++ resources/views/discussions/show.blade.php
@@ -2,12 +2,36 @@
 
 @section('content')
 
-<div class="panel panel-default">
-    <div class="panel-heading">{{ $discussion->title }}</div>
-
-    <div class="panel-body">
-        You are logged in!
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            Created by -
+            <span><b>{{ $discussion->user->name }},</b> {{ $discussion->created_at->diffForHumans() }}</span>
+        </div>
+        <div class="panel-body">
+            <h4 class="text-center"><b>{{ $discussion->title }}</b></h4>
+            <p class="text-center">{{ $discussion->content }}</p>
+        </div>
+        <div class="panel-footer">
+            <p>
+                {{ $discussion->replies->count() }} Replies
+            </p>
+        </div>
     </div>
-</div>
+
+    <!-- display the replies for the discussions -->
+    @foreach ($discussion->replies as $reply)
+        <div class="panel panel-default" style="border-radius: 30px;">
+            <div class="panel-body">
+                <p class="">{{ $reply->content }}</p><hr>
+                <img src="{{ $reply->user->avatar }}" alt="{{ $reply->user->name }}" style="width:25px; height:25px; border-radius:50%;"> <!-- avatar of the person that replied -->
+                &nbsp;&nbsp;&nbsp;
+                <span><b>{{ $reply->user->name }},</b> {{ $reply->created_at->diffForHumans() }}</span>
+
+                <span class="pull-right" style="margin-right: 50px;">
+                    Like
+                </span>
+            </div>
+        </div>
+    @endforeach
 
 @endsection
--- resources/views/forum.blade.php
+++ resources/views/forum.blade.php
@@ -8,11 +8,11 @@
                 Created by -
                 <img src="{{ $discussion->user->avatar }}" alt="{{ $discussion->user->name }}" style="width:30px; height:30px; border-radius:50%;"> <!-- avatar of the person that created the post -->
                 &nbsp;&nbsp;&nbsp;
-                <span>{{ $discussion->user->name }}</span>
+                <span><b>{{ $discussion->user->name }},</b> {{ $discussion->created_at->diffForHumans() }}</span>
                 <a href="{{ route('discussion', ['slug' => $discussion->slug]) }}" class="btn btn-info btn-xs pull-right">Veiw discussion</a>
             </div>
             <div class="panel-body">
-                <h4 class="text-center">{{ $discussion->title }}</h4>
+                <h4 class="text-center"><b>{{ $discussion->title }}</b></h4>
                 <p class="text-center">{{ str_limit($discussion->content, 100) }}</p>  <!-- str_limit() limits that amount of character that will be dislayed -->
             </div>
             <div class="panel-footer">
